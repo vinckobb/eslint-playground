b1c10a546cde8d7b9bd5537eeb47241a887ab22c
vincibb <tomas.vince@dnesne.sk>
2020-06-24 20:58:23 +0200

    Initial commit

---
+++ src/app/test.component.ts
@@ -0,0 +1,18 @@
+export class TestClass
+{
+    //######################### constructor #########################
+
+    constructor()
+    {}
+
+    //######################### public methods #########################
+
+    /**
+     * Sum of two numbers
+     * @param a number one
+     * @param b number two
+     */
+    public sum(a: number, b: number) {
+        return a + b;
+    }
+}
